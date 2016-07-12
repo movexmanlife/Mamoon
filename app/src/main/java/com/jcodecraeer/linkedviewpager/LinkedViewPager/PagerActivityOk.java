e920cff5f62a8d2124ce07aa597fce43d7a61b2f
keyinchang <kyc@qian360.com>
2016-07-12 16:38:30 +0800

    (1)add follow viewpager,but soomth bug;

--- app/src/main/java/com/jcodecraeer/linkedviewpager/LinkedViewPager/PagerActivityOk.java
+++ app/src/main/java/com/jcodecraeer/linkedviewpager/LinkedViewPager/PagerActivityOk.java
@@ -1,18 +1,18 @@
 package com.jcodecraeer.linkedviewpager.LinkedViewPager;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.*;
-import android.support.v4.view.ViewPager;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
-import android.widget.LinearLayout;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
 import io.lahphim.mamoon.R;
@@ -20,16 +20,43 @@ import io.lahphim.mamoon.R;
 /**
  * 成功的实现了TextView显示不同的日期
  */
-public class PagerActivity extends FragmentActivity {
+public class PagerActivityOk extends FragmentActivity {
     private RelativeLayout[] relativeLayouts;
     private int mCurrentYearMonthPosition = Integer.MAX_VALUE / 2;
+    private ViewPager main_scrolllayout;
+    private MyPagerAdapter mFramePageAdapter;
+    private ArrayList<View> mFramePageViews;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pager);
+        setContentView(R.layout.activity_pager_ok);
 
-        final MultiViewPager pager = (MultiViewPager) findViewById(R.id.pager);
+        final MultiViewPagerOk pager = (MultiViewPagerOk) findViewById(R.id.pager);
+        main_scrolllayout = (ViewPager) findViewById(R.id.main_scrolllayout);
+
+        LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View frameView1 = inflater.inflate(R.layout.transparent_layer_image, null);
+        initFramePagerView(frameView1 ,R.drawable.frame_view1);
+        View frameView2 = inflater.inflate(R.layout.transparent_layer_image, null);
+        initFramePagerView(frameView2 ,R.drawable.frame_view2);
+        View frameView3 = inflater.inflate(R.layout.transparent_layer_image, null);
+        initFramePagerView(frameView3 ,R.drawable.frame_view3);
+        View frameView4 = inflater.inflate(R.layout.transparent_layer_image, null);
+        initFramePagerView(frameView4 ,R.drawable.frame_view4);
+        View frameView5 = inflater.inflate(R.layout.transparent_layer_image, null);
+        initFramePagerView(frameView5 ,R.drawable.frame_view5);
+
+        mFramePageViews = new ArrayList<>();
+        mFramePageViews.add(frameView1);
+        mFramePageViews.add(frameView2);
+        mFramePageViews.add(frameView3);
+        mFramePageViews.add(frameView4);
+        mFramePageViews.add(frameView5);
+
+        mFramePageAdapter = new MyPagerAdapter(mFramePageViews);
+        main_scrolllayout.setAdapter(mFramePageAdapter);
 
         final MyAdapter adapter = new MyAdapter();
         pager.setAdapter(adapter);
@@ -39,9 +66,17 @@ public class PagerActivity extends FragmentActivity {
         for(int i=0; i< relativeLayouts.length; i++){
             relativeLayouts[i] = (RelativeLayout)LayoutInflater.from(this).inflate(R.layout.date_page, null);
         }
+
+        main_scrolllayout.setFollowViewPager(pager);
     }
 
-    public class MyAdapter extends android.support.v4.view.PagerAdapter {
+    public void initFramePagerView(View frameView ,int drawable){
+        ImageView image = (ImageView)frameView.findViewById(R.id.image);
+        image.setImageResource(drawable);
+
+    }
+
+    public class MyAdapter extends PagerAdapter {
 
         @Override
         public int getCount() {
